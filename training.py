7a63176ee8a19803dd661a1bbda4391e8f3a5991
abhikesare9 <abhikesare9@gmail.com>
2024-01-28 15:08:38 +0530

    upload to storage

--- training.py
+++ training.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import nltk
 from nltk.stem.porter import PorterStemmer
+
+from google.cloud import storage
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 STOPWORDS = set(stopwords.words('english'))
@@ -21,8 +23,22 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 import pickle
 import re
+import os
 
 dataset = pd.read_csv('dataset_balanced_clean_mv.csv')
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'celtic-house-412214-b0285d141708.json'
+def upload_cs_file(bucket_name, source_file_name, destination_file_name): 
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(destination_file_name)
+    blob.upload_from_filename(source_file_name)
+
+    return True
+
+
 
 print('loaded dataset')
 dataset = dataset.dropna()
@@ -49,6 +65,8 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset['feedback'].values
 pickle.dump(cv, open('countVectorizer.pkl', 'wb'))
 
+upload_cs_file('amazon-sentimental-analysis-data-iks','countVectorizer.pkl','models/countVectorizer.pkl')
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 15)
 
@@ -59,6 +77,7 @@ X_test_scl = scaler.transform(X_test)
 
 
 pickle.dump(scaler, open('scaler.pkl', 'wb'))
+upload_cs_file('amazon-sentimental-analysis-data-iks','scaler.pkl','models/scaler.pkl')
 
 
 model_xgb = XGBClassifier()
@@ -69,3 +88,5 @@ print("Training Accuracy :", model_xgb.score(X_train_scl, y_train))
 print("Testing Accuracy :", model_xgb.score(X_test_scl, y_test))
 
 pickle.dump(model_xgb, open('model_xgb.pkl', 'wb'))
+
+upload_cs_file('amazon-sentimental-analysis-data-iks','model_xgb.pkl','models/model_xgb.pkl')
